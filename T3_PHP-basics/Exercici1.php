e4925662f36bd0c9398754f19412a222c03a0725
Jordi <jordijs6@gmail.com>
2022-09-28 13:19:32 +0200

    Actualitzats els arxius amb el nou sistema de carpetes

---
+++ T3_PHP-basics/Exercici1.php
@@ -0,0 +1,8 @@
+<?php 
+    $age = 28;
+    $commaNumber = 123.456;
+    $name = "Jordi";
+    $isStudent = true;
+
+    echo "My age is ", $age, ", my double is ", $commaNumber, ", my name is ", $name, " and my student status is: ", $isStudent;
+?>
