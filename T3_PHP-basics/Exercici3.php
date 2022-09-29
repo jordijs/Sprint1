81524183455791d76e5461b6ef442ed79946d555
Jordi Julià <jordijs6@gmail.com>
2022-09-29 19:24:20 +0200

    Completat Exercici 3 PHP

---
+++ T3_PHP-basics/Exercici3.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Exercici 3</title>
+    </head>
+    <body>
+        <?php
+        define ("NAME", "Jordi");
+        echo "<h1><strong>" . NAME . "</strong></h1>";
+        ?>
+    </body>
+</html>
