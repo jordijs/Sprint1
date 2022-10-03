df6399e97271b79f9069cd261f23507d31a6dd55
Jordi <jordijs6@gmail.com>
2022-10-03 13:58:36 +0200

    Completat Exercici 1 de Funcions PHP

--- T3_Funcions-Estructures/Exercici1.php
+++ T3_Funcions-Estructures/Exercici1.php
@@ -2,7 +2,17 @@
 <html>
     <body>
         <?php
-            function even ()
+            function even($age)
+            {
+                if (($age % 2) == 0) {
+                    return "L'edat $age és parell";
+                } else {
+                    return "L'edat $age és senar";
+                }
+            }
+
+            echo even (28);
+        
         ?>
     </body>
 </html>
